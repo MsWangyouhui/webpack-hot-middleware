c4a32001679bf953ea4f50372d8d7157b7268ae7
MsWangyouhui <ponianrsheng@foxmail.com>
2017-08-08 18:23:38 +0800

    学习过程

---
+++ example/webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: [
+    // Add the client which connects to our middleware
+    // You can use full urls like 'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr'
+    // useful if you run your app from another point like django
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+    // And then the actual application
+    './client.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/test',
+    filename: 'bundle.js'
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+};
